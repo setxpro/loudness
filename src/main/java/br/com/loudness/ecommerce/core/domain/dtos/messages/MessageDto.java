9681880e38d74eabcca79daa1e3d67088b85eaea
Patrick Anjos <patrickpqdt87289@gmail.com>
2023-10-18 15:43:52 -0300

    fix(message): success object message

---
+++ src/main/java/br/com/loudness/ecommerce/core/domain/dtos/messages/MessageDto.java
@@ -0,0 +1,14 @@
+package br.com.loudness.ecommerce.core.domain.dtos.messages;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageDto {
+    private String mesage;
+}
