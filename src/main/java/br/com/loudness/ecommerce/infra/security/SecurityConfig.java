783b7278a130e609d75268ba26e63b9ffe776d7a
Patrick Anjos <patrickpqdt87289@gmail.com>
2023-10-18 10:55:17 -0300

    fix(requestMatchers): to singin

--- src/main/java/br/com/loudness/ecommerce/infra/security/SecurityConfig.java
+++ src/main/java/br/com/loudness/ecommerce/infra/security/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers((HttpMethod.POST), "/collaborator").permitAll()
+                        .requestMatchers((HttpMethod.POST), "/auth/login").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class) // add filter
