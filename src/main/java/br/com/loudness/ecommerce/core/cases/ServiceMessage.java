e7cfec81c4e344b0a168d65fd4f21bb1ea1a6350
Patrick Anjos <patrickpqdt87289@gmail.com>
2023-10-18 10:58:51 -0300

    feat(ObjectMessage): class

---
+++ src/main/java/br/com/loudness/ecommerce/core/cases/ServiceMessage.java
@@ -0,0 +1,14 @@
+package br.com.loudness.ecommerce.core.cases;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ServiceMessage {
+    private String message;
+}
